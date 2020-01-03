08e237e152655005fdaa7c64e4752a17bebeeccd
= <chase.terry87@gmail.com>
2020-01-03 13:28:19 -0600

    Added dependencies for websocket integration.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,6 +5,17 @@ window._ = require('lodash');
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+import Echo from "laravel-echo"
+window.Pusher = require('pusher-js');
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: 'dddc11865361a641b70f',
+    wsHost: window.location.hostname,
+    wsPort: 6001,
+    disableStats: true,
+});
+
+
 document.addEventListener('DOMContentLoaded', () => {
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
@@ -12,20 +23,20 @@ document.addEventListener('DOMContentLoaded', () => {
     // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
 
-      // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
+        // Add a click event on each of them
+        $navbarBurgers.forEach( el => {
+            el.addEventListener('click', () => {
 
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
 
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
 
+            });
         });
-      });
     }
 
 });
