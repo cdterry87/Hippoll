6b9eaf6689f9b0227ea755b4dbda57761617d3c6
= <chase.terry87@gmail.com>
2020-01-03 21:47:09 -0600

    Created a user response event and changed the pusher key to pull from env.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,7 +9,7 @@ import Echo from "laravel-echo"
 window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: 'dddc11865361a641b70f',
+    key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     wsPort: 6001,
     disableStats: true,
