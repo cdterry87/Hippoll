23f95c8ced276a2560f831daea40d730f37e0e94
= <chase.terry87@gmail.com>
2020-01-09 16:10:54 -0600

    Adjusted some settings for websockets.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,14 +6,18 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import Echo from "laravel-echo"
+
 window.Pusher = require('pusher-js');
+
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     wsPort: 6001,
     disableStats: true,
+    forceTLS: true
 });
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
